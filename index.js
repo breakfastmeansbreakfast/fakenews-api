411271f0d59bcc96e66c9f1206bd7edbb2c75a87
breakfastmeansbreakfast <77744c@gmail.com>
2018-10-24 13:53:51 +0100

    db connection working

---
+++ index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+require('dotenv').config({
+  path: path.join(__dirname, './settings.env'),
+});
+
+const app = express();
+mongoose.connect(process.env.DATABASE_CONN, { useNewUrlParser: true });
+app.use(bodyParser.json())
+app.get('/', (req, res) => res.send('Hello MongoDb!'));
+
+app.listen(3000, () => console.log('It works!'));
+
