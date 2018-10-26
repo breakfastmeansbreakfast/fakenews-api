4c45887ae056fa63288ffd56985d5841b63511d2
breakfastmeansbreakfast <77744c@gmail.com>
2018-10-26 17:49:41 +0100

    question rendering

--- controllers/Question.js
+++ controllers/Question.js
@@ -1,9 +1,7 @@
 const Question = require('../models/Question');
 
 exports.list = (req, res) => {
- // console.log(req)
   Question.find({}, (err, question) => {
-    console.log(question)
     if (err) {
       res.json('Something went wrong');
     }
@@ -11,14 +9,14 @@ exports.list = (req, res) => {
   });
 };
 
-/*exports.get = (req, res) => {
-  Question.findById(req.params.question_id, (err, question) => {
+exports.displayQuestion = (req, res) => {
+  Question.find({ headline: 'CHECK OUT THIS FUCKING HEADLINE' }, (err, question) => {
     if (err) {
-      res.json('something went wrong...');
+      res.json('Something went wrong');
     }
     res.json(question);
   });
-};*/
+};
 
 module.exports.list = exports.list;
-//module.exports.get = exports.get;
+module.exports.displayQuestion = exports.displayQuestion;
--- index.js
+++ index.js
@@ -2,19 +2,21 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-//const adduser = require('./controllers/Adduser');
+const adduser = require('./controllers/Adduser');
 const getquestion = require('./controllers/Question');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
 
 require('dotenv').config({
   path: path.join(__dirname, './settings.env'),
 });
 
-const app = express();
 mongoose.connect(process.env.DATABASE_CONN, { useNewUrlParser: true });
 app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('Hello MongoDb!'));
-//app.post('/adduser', adduser.post);
-app.get('/question', getquestion.list);
-//app.get('/question/:question_id', getquestion.get);
+app.post('/adduser', adduser.post);
+app.get('/question', getquestion.displayQuestion);
 
-app.listen(3000, () => console.log('It works!'));
+app.listen(9999, () => console.log('It works!'));
